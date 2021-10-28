8a5dee05e3b91df51d528b28672f9234e9d4b311
jayanthnaramsetty <naramsetty.jc@gmail.com>
2021-10-27 22:00:31 -0500

    changes made

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var digi = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  digi = digi + 1;
+  res.send(`User accesses are: ${digi}`);
 });
 
 module.exports = router;
